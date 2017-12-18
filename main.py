f818f0d1059729ed149248bb269d45f53797392c
Michael Sinov <sihaelov@gmail.com>
2017-12-18 22:03:46 +1000

    Add API

--- main.py
+++ main.py
@@ -17,10 +17,14 @@ async def index(request):
     return response
 
 
-async def run_sql(request):
+async def api_run_sql(request):
 
     data = await request.post()
-    sql_query = data.get('sql')
+    sql_query = data.get('query')
+
+    if not sql_query:
+        return web.json_response({'headers': [], 'rows': [],
+                                  'error': "Empty query"})
 
     db_url = 'Driver={ODBC Driver 13 for SQL Server};Server=sql.steemsql.com;Database=DBSteem;uid=steemit;pwd=steemit'
     with pyodbc.connect(db_url, timeout=60) as connection:
@@ -28,24 +32,27 @@ async def run_sql(request):
         # connection.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
         # connection.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
         error = None
+        results = []
         try:
             rows_raw = cursor.execute(sql_query).fetchmany(5000)
-            rows = [list(row_raw) for row_raw in rows_raw]
             headers = [header[0] for header in cursor.description]
+
+            for row in rows_raw:
+                results.append(dict(zip(headers, row)))
+
         except Exception as e:
             print('Error')
             error = str(e)
-            rows = []
             headers = []
 
-    rows_json = json.dumps(rows, default=str)
+    rows_json = json.dumps(results, default=str)
     return web.json_response({'headers': headers, 'rows': rows_json,
                               'error': error})
 
 
 app = web.Application()
 app.router.add_get('/', index)
-app.router.add_post('/query/', run_sql)
+app.router.add_post('/', api_run_sql)
 
 # ~/Projects/steemit/sql/aio
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
